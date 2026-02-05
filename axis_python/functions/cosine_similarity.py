441c3a21dbf79817262fa4c80e77eb8b2642f43b
ddiazsouto@gmail.com <ddiazsouto@gmail.com>
2026-02-05 12:58:57 +0000

    Update epsilon in the cosine similarity in order to match the epsilon fom the transformers normalisation layer

--- axis_python/functions/cosine_similarity.py
+++ axis_python/functions/cosine_similarity.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 
-def cosine_similarity(
-        vector1: np.ndarray, vector2: np.ndarray) -> float:
+def cosine_similarity(vector1: np.ndarray, vector2: np.ndarray) -> float:
+    epsilon = 1e-12
     denominator = (
-        np.linalg.norm(vector1) * np.linalg.norm(vector2) + 1e-8
+        np.linalg.norm(vector1) * np.linalg.norm(vector2) + epsilon
     )
     return np.dot(vector1, vector2) / denominator
